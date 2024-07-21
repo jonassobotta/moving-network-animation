8c88ec3550e5a3978ebbf19aedf6087fa0e80894
jonassobotta <Jonas.sobotta01@gmail.com>
2024-07-21 14:51:42 +0200

    update package.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './path/to/umdWrapper.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'moving-network-animation.bundle.js',
-        library: 'MovingNetworkAnimation',
-        libraryTarget: 'umd',
-        globalObject: 'this'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'MovingNetworkAnimation',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
